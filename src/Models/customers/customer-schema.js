b026ae09adff2d95fd82310720ba93af8b1e2ac9
Vraj <vraj.patel4801@gmail.com>
2023-03-11 01:29:56 +0530

    unusal commit:)

--- src/Models/customers/customer-schema.js
+++ src/Models/customers/customer-schema.js
@@ -3,44 +3,15 @@ const jwt = require("jsonwebtoken");
 
 const CustomerSchema = new mongoose.Schema({
 
-    customer_email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-
-    customer_name: {
-        type: String,
-    },
-
-    customer_image: {
-        type: String
-    },
-
-    customer_mobile: {
-        type: Number,
-        minlength: [10, "Mobile number must be of 10 digits"],
-        maxlength: [10, "Mobile number must be of 10 digits"]
-    },
-
-    customer_homeNo: {
-        type: Number,
-    },
-    customer_street: {
-        type: String,
-    },
-    customer_pincode: {
-        type: Number,
-        minlength: 6,
-        maxlength: 6
-    },
-    customer_district: {
-        type: String,
-    },
-    customer_state: {
-        type: String,
-    },
-
+    customer_email: { type: String, required: true, unique: true },
+    customer_name: { type: String, },
+    customer_image: { type: String },
+    customer_mobile: { type: Number, minlength: [10, "Mobile number must be of 10 digits"], maxlength: [10, "Mobile number must be of 10 digits"] },
+    customer_homeNo: { type: Number, },
+    customer_street: { type: String, },
+    customer_pincode: { type: Number, minlength: 6, maxlength: 6 },
+    customer_district: { type: String, },
+    customer_state: { type: String, },
     loginTokens: [{
         token: {
             type: String,
@@ -49,9 +20,7 @@ const CustomerSchema = new mongoose.Schema({
         }
     }],
 },
-    {
-        timestamps: true
-    }
+    { timestamps: true }
 );
 
 CustomerSchema.methods.createToken = async function () {
