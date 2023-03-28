057793f74d0cd87d8820d0e0dccd2a2166b9e642
vrajpatel <vraj.patel4801@gmail.com>
2023-03-28 10:01:02 +0530

    con-1:)

--- Src/DB/connection.js
+++ Src/DB/connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.set("strictQuery", true);
 
-mongoose.connect("mongodb://localhost:27017/ShreeSahajanandFurniture")
+mongoose.connect("mongodb+srv://vrajpatel:FMdrLuFJqgxszGfA@cluster0.mpqkx.mongodb.net/SSF?retryWrites=true&w=majority")
     .then(() => { console.log(`db connection successfully...`); })
     .catch((err) => { console.log(err); });
