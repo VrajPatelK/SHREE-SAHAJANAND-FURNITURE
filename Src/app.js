4f58efb6b43426369fd98780b20d71a925e58627
yashvipatel <patelyashvi638@gmail.com>
2023-03-29 16:42:06 +0530

    partial commit for payment gateway:)

--- Src/app.js
+++ Src/app.js
@@ -224,6 +224,69 @@ app.get('/autocomplete/', (req, res) => {
 
 app.post("/filter-products", product_read_routers);
 
+//payments
+
+const cors = require("cors");
+app.use(cors());
+
+// const paymentRoutes = require("./Routes/payment");
+// const { payment } = require("./Helpers/payment");
+// app.use("/api/payment/");
+
+
+
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
+
+app.post("/api/payment/orders", async (req, res) => {
+    try {
+        const instance = new Razorpay({
+            key_id: process.env.KEY_ID,
+            key_secret: process.env.KEY_SECRET,
+        });
+
+        const options = {
+            amount: req.body.amount * 100,
+            currency: "INR",
+            receipt: crypto.randomBytes(10).toString("hex"),
+        };
+
+        instance.orders.create(options, (error, order) => {
+            if (error) {
+                console.log(error);
+                return res.status(500).json({ message: "Something Went Wrong!" });
+            }
+            res.status(200).json({ data: order });
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error!" });
+        console.log(error);
+    }
+});
+
+app.post("/api/payment/verify", async (req, res) => {
+    try {
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+            req.body;
+        const sign = razorpay_order_id + "|" + razorpay_payment_id;
+        const expectedSign = crypto
+            .createHmac("sha256", process.env.KEY_SECRET)
+            .update(sign.toString())
+            .digest("hex");
+
+        if (razorpay_signature === expectedSign) {
+            return res.status(200).json({ message: "Payment verified successfully" });
+        } else {
+            return res.status(400).json({ message: "Invalid signature sent!" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error!" });
+        console.log(error);
+    }
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`connection successfully... at http://127.0.0.1:${PORT}`);
 });
